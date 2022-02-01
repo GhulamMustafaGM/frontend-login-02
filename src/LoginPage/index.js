c44c38501f156d6c907d8650da0c7493ccfb90c9
Ghulam Mustafa <mustafaji@gmail.com>
2022-02-01 23:22:25 +0100

    Frontend Login Design

--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import bird from './bird.jpg'
+import bird from "./bird.jpg";
 function LoginPage2() {
   return (
     <div className="login-page-2">
@@ -22,34 +22,27 @@ function LoginPage2() {
 
             <div className="d-flex align-items-center justify-content-center">
               <img
-                height='40'
-                width='40'
+                height="40"
+                width="40"
                 className="mx-5"
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 alt=""
               />
-               <img
+              <img
                 className="mx-5"
-                height='30'
-                width='30'
+                height="30"
+                width="30"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"
                 alt=""
               />
             </div>
-             <a href="#" className="mt-3 w-100 text-center">Not Yet Registerd ? Click Here To Sign Up</a>
+            <a href="#" className="mt-3 w-100 text-center">
+              Not Yet Registerd ? Click Here To Sign Up
+            </a>
           </div>
         </div>
         <div className="col-md-5 image-part">
-         
-         
-          <img
-            className="bird-image"
-            src={bird}
-            alt=""
-          />
-         
-
-     
+          <img className="bird-image" src={bird} alt="" />
         </div>
       </div>
     </div>
